698df746b1f4cb2475136fcd5465dd3f7d74c531
Sarah Donvito <sarah.donvito@pagopa.it>
2024-09-09 18:13:54 +0200

    feat(PN-12551): replace first variable in template

--- generateHtmlTemplates.js
+++ generateHtmlTemplates.js
@@ -3,50 +3,42 @@ const fs = require("fs-extra");
 const path = require("path");
 
 async function loadTranslations(language, template) {
-  console.log("siamoq qui pre path");
-
   const filePath = path.join(
     __dirname,
     "templates/" + template,
     "/i18n",
     `${language}.json`
   );
-  console.log("siamoq qui " + filePath);
   return await fs.readJson(filePath);
 }
 
 async function generateHtmlTemplates() {
-  // Definisci le lingue supportate
-  const languages = ["en", "it", "fr", "de"]; // Aggiungi altre lingue se necessario
-  // definisci i template
-  const templates = ["aar", "AO3"];
-  console.log("siamo qui 2");
+  const languages = ["en"]; // Elenca le lingue supportate
+  const templates = [
+    "NotificationAAR_RADDalt",
+    "NotificationReceivedLegalFact",
+    "PecDeliveryWorkflowLegalFactSuccess",
+    "PecDeliveryWorkflowLegalFactError",
+    "NotificationViewedLegalFact",
+    "PdfLegalFact",
+  ];
 
-  // Carica il template HTML di base
   const templatePath = path.join(
     __dirname,
     "templates",
-    "aar",
-    "documento_base.html"
+    templates[0], // sostituisci con la cartella che contiene il file che vuoi generare
+    "index.html"
   );
   const templateContent = await fs.readFile(templatePath, "utf8");
+  const template = templates[0]; // sostituisci col tipo di template che vuoi generare
 
   for (const language of languages) {
-    console.log("siamoq qui " + language);
-
-    const template = templates[0];
-    // Carica le traduzioni per la lingua corrente per il template in esame
-    // es language = de, template = AAR
     const translations = await loadTranslations(language, template);
-
-    // Renderizza il template con le traduzioni
     const renderedHtml = ejs.render(templateContent, translations);
 
-    // Salva il template HTML renderizzato in una cartella di output
-    const outputDir = path.join(__dirname, "output");
-    await fs.ensureDir(outputDir); // Assicura che la cartella di output esista
-
-    const outputPath = path.join(outputDir, `documento_${language}.html`);
+    const outputDir = path.join(__dirname, "output", template);
+    await fs.ensureDir(outputDir);
+    const outputPath = path.join(outputDir, `${template}_${language}.html`);
     await fs.writeFile(outputPath, renderedHtml, "utf8");
 
     console.log(`Template HTML generato per la lingua: ${language}`);
